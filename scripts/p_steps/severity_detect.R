b5a832102fc14397e857db681e6776475f88f7fe
Ema <e.m.alsina-2@umcutrecht.nl>
2022-09-14 18:36:27 +0200

    apply max severity to all observations of covid episode working for single episode.
    need to separate episodes... tricky

--- scripts/p_steps/severity_detect.R
+++ scripts/p_steps/severity_detect.R
@@ -40,11 +40,22 @@ severity_indicators<-c("hospital", "pd", "sd", "intensive", "cause_of_death")
 
 # will STARTSWITH become a problem? rather look for string at any position
 
-sev_rows<-which(Reduce(`|`, lapply(severity_indicators, startsWith, x = as.character(covid_unique$meaning))))
+sev_rows<-which(Reduce(`|`, lapply(severity_indicators, startsWith, x = as.character(covid_data$meaning))))
 
-covid_unique$severe<-0
+covid_data$severe<-0
 
-covid_unique$severe[sev_rows]<-1
+covid_data$severe[sev_rows]<-1
+
+# #only for development!!!
+# # the simulated RTI data has date values that make no sense (before COVID existed... so I'm going to impute dates that will make sense)
+# 
+# cov_start<- as.numeric(as.Date("2020-01-01", format="%Y-%m-%d"))
+# covid_data$cov_date<-cov_start
+# episode_duration<-sample(1:30, nrow(covid_data), replace = T)+sample(c(0,200),nrow(covid_data), replace = T)
+# covid_data$cov_date<-covid_data$cov_date+episode_duration
+# 
+# covid_data$severe<-sample(0:1, nrow(covid_data), replace = T)
+# 
 
 # need to group events --> episodes (any severe indicator with 4 weeks of first date)
 # AAAAAHHHHHHHHHHHH >_<
@@ -52,7 +63,41 @@ covid_unique$severe[sev_rows]<-1
 
 # merge covid episodes
 
-table(table(covid_data$person_id))
+# table(table(covid_data$person_id))
+
+covid_data<-covid_data[with(covid_data, order(person_id, cov_date)),]
+
+covid_grouped<-covid_data%>%group_by(person_id)
+
+
+first_covid<-covid_grouped%>%slice_head()
+last_covid<-covid_grouped%>%slice_tail()
+
+duration<-(last_covid$cov_date)-(first_covid$cov_date)
+summary(duration)
+hist(duration, breaks=30)
+#GOOD this is what we expect
+
+  single_episode_id<-first_covid$person_id[duration<31]
+  multi_episode_id<-first_covid$person_id[duration>30]
+  #check these are distinct cohorts
+  all(single_episode_id%in%multi_episode_id==F)
+  
+  df_covid_single<-covid_data[covid_data$person_id%in%single_episode_id,]
+  id_freq<-as.numeric(table(df_covid_single$person_id))
+  
+  df_covid_single$episode<-1
+  
+  df_covid_single<-df_covid_single[with(df_covid_single, order(person_id, severe)),]
+  
+  max_sev<-df_covid_single%>%group_by(person_id)%>%slice_tail()
+  length(id_freq)==nrow(max_sev)
+  df_covid_single$max_severity<-rep(max_sev$severe,id_freq)
+  
+  df_covid_multi<-covid_data[covid_data$person_id%in%multi_episode_id,]
+  df_covid_multi<-df_covid_multi%>%group_by(person_id)
+  # table(table(df_covid_multi$person_id))
+  
 
 #similar task to create_spells.... 
 
