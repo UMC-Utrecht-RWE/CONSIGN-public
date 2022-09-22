ef657214ef7718dae482bcbd73924328dbe34044
Ema <e.m.alsina-2@umcutrecht.nl>
2022-09-22 17:41:22 +0200

    only using "narrow" codes to detect covid in events

--- scripts/p_steps/covid_detect_function.R
+++ scripts/p_steps/covid_detect_function.R
@@ -35,7 +35,8 @@ NULL
 events_tables<-IMPORT_PATTERN(pat="EVENTS_SLIM", dir = preselect_folder)
 
 codesheet<-fread(paste0(projectFolder,"/ALL_full_codelist.csv"))
-codesheet2<-codesheet[codesheet$event_definition=="COVID19 diagnosis",]
+codesheet1<-codesheet[codesheet$tags=="Narrow",]
+codesheet2<-codesheet1[codesheet1$event_definition=="COVID19 diagnosis",]
 # filter for narrow? 
 
 CreateConceptDatasets <- function(codesheet=codesheet2,file=events_tables, c.voc="coding_system", c.concept="full_name", c.codes="code", c.startwith = NULL,
