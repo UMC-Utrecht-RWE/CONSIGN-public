e68b759f28f2c885fb8b212df0f7cb2b66194329
Ema <e.m.alsina-2@umcutrecht.nl>
2022-10-27 15:41:22 +0200

    added table "0" with the pandemic pregnant cohort description

--- 00_to_run_create_tables_1abc_and_supplement.R
+++ 00_to_run_create_tables_1abc_and_supplement.R
@@ -24,6 +24,7 @@ suppressMessages(source(paste0(pre_dir, "/packages.R")))
 
 source(paste0(pre_dir,"/IMPORT_PATTERN_FUNC.R"))
 
+source(paste0(pre_dir, "/create_table0_cohort_description.R"))
 
 source(paste0(pre_dir, "/create_table_1abc.R"))
 
