244aa57005cedc16481882f2d08ead3ac901ce3c
Ema <e.m.alsina-2@umcutrecht.nl>
2022-10-27 16:23:08 +0200

    tables 2abc

--- 00_to_run_create_tables_2abcdef.R
+++ 00_to_run_create_tables_2abcdef.R
@@ -2,7 +2,7 @@
 
 # matched cohort drug utilization
 
-# 3 d e and f are trim 1, 2, 3 covid+ non-preg matched cohort drug utilization
+# 2 d e and f are trim 1, 2, 3 pregnant matched cohort drug utilization
 
 
 rm(list=ls())
@@ -22,10 +22,10 @@ suppressMessages(source(paste0(pre_dir, "/packages.R")))
 
 source(paste0(pre_dir,"/IMPORT_PATTERN_FUNC.R"))
 
-
+source(paste0(pre_dir, "/create_table_2abc.R"))
 
 source(paste0(pre_dir, "/cov_window_exposure_function.R"))
 
-source(paste0(pre_dir, "/trimester_drug_exposure_NP_matches.R"))
+source(paste0(pre_dir, "/trimester_drug_exposure_PREG_matches.R"))
 
-source(paste0(pre_dir, "/create_table_3def.R"))
+source(paste0(pre_dir, "/create_table_2def.R"))
