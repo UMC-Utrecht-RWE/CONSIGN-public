480cbf5be2ac844d9200c05739fb2eb522c79bb7
Ema <e.m.alsina-2@umcutrecht.nl>
2022-10-31 14:01:09 +0100

    timing for covariates (at least one covariate date within 1 year before covid infection)

--- 04_to_run_covariates.R
+++ 04_to_run_covariates.R
@@ -34,32 +34,36 @@ DAP<-CDM_source$data_access_provider_name
 if(DAP!="Bordeaux"){
   source(paste0(pre_dir,"/covariates_detect.R" ))}else{source(paste0(pre_dir,"/covariates_detect_BPE.R" ))}
 
+source(paste0(pre_dir,"/covi_covariates_timing.R" ))
+
 # covariate timing combining to pregnancy/covid
 
-if(DAP=="ARS"){
-  source(paste0(pre_dir,"/ARS_mat_cov_no_link.R" ))}
-  
-if(DAP=="Aarhus"){
-  source(paste0(pre_dir,"/Aarhus_mat_cov_no_link.R" ))}
+source(paste0(pre_dir,"/preterm_birth.R" ))
 
-# only using data from matched cohorts... so bordeaux data SHOULD load with IMPORT_PATTERN
-if(DAP=="Bordeaux"){
-  source(paste0(pre_dir,"/Bordeaux_mat_cov_no_link.R" ))}
-
-if(DAP=="TEST"){
-  source(paste0(pre_dir,"/Bordeaux_mat_cov_no_link.R" ))}
-
-if(DAP=="FISABIO"){
-  source(paste0(pre_dir,"/Fisabio_mat_cov_no_link.R" ))}
-
-if(DAP=="IACS"){
-  source(paste0(pre_dir,"/IACS_mat_cov_no_link.R" ))}
-
-if(DAP=="Karolinska"){
-  source(paste0(pre_dir,"/Karolinska_mat_cov_no_link.R" ))}
-
-if(DAP=="UOSL"){
-  source(paste0(pre_dir,"/Oslo_mat_cov_no_link.R" ))}
-
-if(DAP=="USWAN"){
-  source(paste0(pre_dir,"/SWANSEA_mat_cov_no_link.R" ))}
+# if(DAP=="ARS"){
+#   source(paste0(pre_dir,"/ARS_mat_cov_no_link.R" ))}
+#   
+# if(DAP=="Aarhus"){
+#   source(paste0(pre_dir,"/Aarhus_mat_cov_no_link.R" ))}
+# 
+# # only using data from matched cohorts... so bordeaux data SHOULD load with IMPORT_PATTERN
+# if(DAP=="Bordeaux"){
+#   source(paste0(pre_dir,"/Bordeaux_mat_cov_no_link.R" ))}
+# 
+# if(DAP=="TEST"){
+#   source(paste0(pre_dir,"/Bordeaux_mat_cov_no_link.R" ))}
+# 
+# if(DAP=="FISABIO"){
+#   source(paste0(pre_dir,"/Fisabio_mat_cov_no_link.R" ))}
+# 
+# if(DAP=="IACS"){
+#   source(paste0(pre_dir,"/IACS_mat_cov_no_link.R" ))}
+# 
+# if(DAP=="Karolinska"){
+#   source(paste0(pre_dir,"/Karolinska_mat_cov_no_link.R" ))}
+# 
+# if(DAP=="UOSL"){
+#   source(paste0(pre_dir,"/Oslo_mat_cov_no_link.R" ))}
+# 
+# if(DAP=="USWAN"){
+#   source(paste0(pre_dir,"/SWANSEA_mat_cov_no_link.R" ))}
