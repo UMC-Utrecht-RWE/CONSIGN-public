6feea1671a743e50ef94e137318c1b5b8b67c81f
Ema <e.m.alsina-2@umcutrecht.nl>
2022-11-02 16:09:26 +0100

    pregnancy filter *ready* cohort elligibility error found by olga fixed

--- scripts/p_steps/pregnancy_filter.R
+++ scripts/p_steps/pregnancy_filter.R
@@ -92,29 +92,13 @@ table(df_PERSONS$pregnant_ever)
 
 # add elligibility criteria from CreateEntryExit (in PERSONS) to my_PREG
 my_PREG$elligible_hist<-0
-elligible_hist_ID<-df_PERSONS$person_id[df_PERSONS$elligible_historical==1]
-length(elligible_hist_ID)
-my_PREG$elligible_hist[my_PREG$person_id%in%elligible_hist_ID]<-1
-table(my_PREG$elligible_hist)
+my_PREG$elligible_hist[between(my_PREG$age_at_start_of_pregnancy, 12,55)]<-1
 
 my_PREG$elligible_pandemic<-0
-ell_pan_preg_id<-df_PERSONS$person_id[df_PERSONS$elligible_pandemic==1]
-my_PREG$elligible_pandemic[my_PREG$person_id%in%ell_pan_preg_id]<-1
+my_PREG$elligible_pandemic[between(my_PREG$age_at_start_of_pregnancy, 12,55)]<-1
+
+table(my_PREG$elligible_hist)
 table(my_PREG$elligible_pandemic)
-# NEVER PREG ONLY IF ELLIGIBLE FOR PANDEMIC COHORT add entry/exit criteria here
-
-never_PREG_ID<-df_PERSONS$person_id[df_PERSONS$pregnant_ever==0 & df_PERSONS$elligible_pandemic==1]
-FC_never_preg<-length(never_PREG_ID)
-
-# NOT pregnant in 2020 from march-december (preg_end_date< jan 1 2020) --> non_preg cohort
-non_pan_preg_ID<- my_PREG$person_id[my_PREG$pregnancy_end_date<historical_end_date &my_PREG$elligible_pandemic==1]
-length(unique(non_pan_preg_ID))
-
-all_non_pan_preg_ID<-unique(c(never_PREG_ID, non_pan_preg_ID))
-FC_all_non_pan_preg<-length(all_non_pan_preg_ID)
-
-
-
 #######################
 # make categorical maternal_age (at start of pregnancy) groups: SAP page 30 "covariates"
 # 12-24 years of age
@@ -133,7 +117,7 @@ table(my_PREG$age_group)
 hist(my_PREG$age_at_start_of_pregnancy)
 
 # establish pregnancy cohorts (historical or pandemic)
-#help- eimir should this be based on start or end of pregnancy?
+#help- eimir should this be based on start or end of pregnancy? 
 
 my_PREG$cohort<-NA
 
@@ -143,6 +127,19 @@ my_PREG$cohort[(my_PREG$pregnancy_end_date>=covid_start_date)]<-"pandemic"
 
 my_PREG$cohort[between(my_PREG$pregnancy_end_date, historical_end_date, covid_start_date) ]<-"between"
 
+
+# NEVER PREG ONLY IF ELLIGIBLE FOR PANDEMIC COHORT add entry/exit criteria here
+
+never_PREG_ID<-df_PERSONS$person_id[df_PERSONS$pregnant_ever==0]
+FC_never_preg<-length(never_PREG_ID)
+
+# NOT pregnant in 2020 from march-december (preg_end_date< jan 1 2020) --> non_preg cohort
+pandemic_id<-my_PREG$person_id[my_PREG$cohort=="pandemic"]
+non_pan_preg_PERSONS<-PERSONS[PERSONS$person_id%exclude%pandemic_id,]
+non_pan_preg_PERSONS_age<-non_pan_preg_PERSONS[between(non_pan_preg_PERSONS$year_of_birth, 1965, 2008),]
+
+fwrite(non_pan_preg_PERSONS_age, paste0(not_preg_folder, "PERSONS.csv"))
+FC_all_non_pan_preg<-nrow(non_pan_preg_PERSONS_age)
 
 
 # all the pregnancies included in the study with additional variables:
@@ -193,7 +190,7 @@ table_list<-unlist(actual_tables_preselect)
 
 # PANDEMIC PREGNANT
 
-pan_preg_ID_age<-my_PREG$person_id[(my_PREG$cohort=="pandemic")& (my_PREG$elligible_pandemic==1)]
+pan_preg_ID_age<-unique(my_PREG$person_id[(my_PREG$cohort=="pandemic")& (my_PREG$elligible_pandemic==1)])
 
 
 for (i in 1:length(table_list)){
@@ -203,13 +200,12 @@ for (i in 1:length(table_list)){
   fwrite(my_preg_table,paste0(pan_preg_folder,table_list[i]))
 }
 
-pan_PREGNANCIES_age<-nrow(my_PREG[my_PREG$person_id%in%pan_preg_ID_age,])
+pan_PREGNANCIES_age<-length(pan_preg_ID_age)
 fwrite(my_PREG[((my_PREG$person_id%in%pan_preg_ID_age)&(my_PREG$cohort=="pandemic")),],paste0(pan_preg_folder,"my_PREG.csv"))
 
 # NON-PREGNANT 
 # need to be 12-55 at start of PANDEMIC (only use this group for cov+ comparison)
-age_pan_ID<-df_PERSONS$person_id[df_PERSONS$elligible_pandemic==1]
-non_pan_preg_ID_age<-all_non_pan_preg_ID[all_non_pan_preg_ID%in%age_pan_ID]
+non_pan_preg_ID_age<-non_pan_preg_PERSONS_age$person_id
 
 for (i in 1:length(table_list)){
   my_table<-fread(paste0(preselect_folder,table_list[i]))
@@ -220,15 +216,14 @@ for (i in 1:length(table_list)){
 # HISTORICAL PREGNANT
 # historical group entry/exit filter for observation time 2018-2020
 
-age_hist_ID<-df_PERSONS$person_id[df_PERSONS$elligible_historical==1]
-hist_preg_ID_age<-hist_preg_ID[hist_preg_ID%in%age_hist_ID]
-
+hist_preg_ID_age<-my_PREG$person_id[my_PREG$cohort=="historical"&my_PREG$elligible_hist==1]
+hist_preg_ID_age<-hist_preg_ID_age[is.na(hist_preg_ID_age)==F]
 for (i in 1:length(table_list)){
   my_table<-fread(paste0(preselect_folder,table_list[i]))
   my_preg_table<- my_table[my_table$person_id%in%hist_preg_ID_age,]
   fwrite(my_preg_table,paste0(hist_preg_folder,table_list[i]))
 }
-fwrite(my_PREG[my_PREG$person_id%in%hist_preg_ID_age,],paste0(hist_preg_folder,"my_PREG.csv"))
+fwrite(my_PREG[my_PREG$cohort=="historical"&my_PREG$elligible_hist==1,],paste0(hist_preg_folder,"my_PREG.csv"))
 
 
 # #######################################################
@@ -237,12 +232,12 @@ fwrite(my_PREG[my_PREG$person_id%in%hist_preg_ID_age,],paste0(hist_preg_folder,"
 
 flowchart<-as.data.frame(cbind(FC_OG_person_ID, FC_OG_preg, FC_OG_mom, FC_exclude_44, FC_no_red_preg,
                                FC_sufficient_follow_up,   length(unique(pan_preg_ID)), length(unique(pan_preg_ID_age)),pan_PREGNANCIES_age,
-                               length(hist_preg_ID),length(hist_preg_ID_age), length(between_preg_ID), FC_never_preg,length(unique(non_pan_preg_ID)), FC_all_non_pan_preg))
+                               length(hist_preg_ID),length(hist_preg_ID_age), length(between_preg_ID), FC_never_preg, FC_all_non_pan_preg))
                          
  colnames(flowchart)<-c("All women of reproductive age between 2018-2021", "total pregnancies","total mothers", "pregnancies excluded due to gestation>44 weeks","after excluding red pregnancies",
                         "pregnancies starting at least 12 months before end of study",   
                         "WOMEN with pandemic pregnancies","WOMEN with pandemic pregnancies with age 12-55 in 2020","number of included pandemic PREGNANCIES", "women with historical pregnancies",
-                        "women with historical pregnancies with age 12-55 in 2018","women with between pregnancies", "women with no pregnancies EVER", "women with no pregnancy DURING pandemic", "women with no pregnancy DURING pandemic age 12-55 in 2020") 
+                        "women with historical pregnancies with age 12-55 in 2018","women with between pregnancies", "women with no pregnancies EVER", "women with no pregnancy DURING pandemic age 12-55 in 2020") 
  
  fwrite(flowchart, paste0(output_dir,"flowchart_study_pop.csv"))
  
