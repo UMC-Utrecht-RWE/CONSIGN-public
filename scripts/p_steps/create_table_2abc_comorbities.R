831b66a5a5cd087100860fb09cf82741aaafc7b5
Ema <e.m.alsina-2@umcutrecht.nl>
2022-11-05 13:14:51 +0100

    "any" comorbidity row created
    "vaccine" row handled separately and added at the end (not part of the "any" comorbidity)

--- scripts/p_steps/create_table_2abc_comorbities.R
+++ scripts/p_steps/create_table_2abc_comorbities.R
@@ -18,7 +18,7 @@ DAP<-CDM_source$data_access_provider_name
 
 case_data<-fread(paste0(output_cov_cases, "cases.csv"))
 # vaccination separate
-case_vaccine<-case_data$vaccine
+case_vaccine<-case_data[,c("severity", "covid_trimester","vaccine")]
 case_data<-case_data[,-"vaccine"]
 
 # create "any comorbidity" column
@@ -31,15 +31,25 @@ case_data_a<-case_data[(case_data$covid_trimester==1),]
 case_data_severe_a<-case_data_a[case_data_a$severity==1,]
 case_data_nonsevere_a<-case_data_a[case_data_a$severity==0,]
 
+case_vaccine_a<-case_vaccine[(case_vaccine$covid_trimester==1),]
+case_vaccine_severe_a<-case_vaccine_a[case_vaccine_a$severity==1,]
+case_vaccine_nonsevere_a<-case_vaccine_a[case_vaccine_a$severity==0,]
 
 case_data_b<-case_data[case_data$covid_trimester==2,]
 case_data_severe_b<-case_data_b[case_data_b$severity==1,]
 case_data_nonsevere_b<-case_data_b[case_data_b$severity==0,]
 
+case_vaccine_b<-case_vaccine[(case_vaccine$covid_trimester==2),]
+case_vaccine_severe_b<-case_vaccine_b[case_vaccine_b$severity==1,]
+case_vaccine_nonsevere_b<-case_vaccine_b[case_vaccine_b$severity==0,]
 
 case_data_c<-case_data[case_data$covid_trimester==3,]
 case_data_severe_c<-case_data_c[case_data_c$severity==1,]
 case_data_nonsevere_c<-case_data_c[case_data_c$severity==0,]
+
+case_vaccine_c<-case_vaccine[(case_vaccine$covid_trimester==3),]
+case_vaccine_severe_c<-case_vaccine_c[case_vaccine_c$severity==1,]
+case_vaccine_nonsevere_c<-case_vaccine_c[case_vaccine_c$severity==0,]
 
 ###########################################
 # remove non-covariate columns
@@ -56,10 +66,24 @@ case_data_c<-case_data_c[,c(-1,-2,-3)]
 case_data_severe_c<-case_data_severe_c[,c(-1,-2,-3)]
 case_data_nonsevere_c<-case_data_nonsevere_c[,c(-1,-2,-3)]
 
+###############################################################
+
+case_vaccine_a<-case_vaccine_a[,c(-1,-2)]
+case_vaccine_severe_a<-case_vaccine_severe_a[,c(-1,-2)]
+case_vaccine_nonsevere_a<-case_vaccine_nonsevere_a[,c(-1,-2)]
+
+case_vaccine_b<-case_vaccine_b[,c(-1,-2)]
+case_vaccine_severe_b<-case_vaccine_severe_b[,c(-1,-2)]
+case_vaccine_nonsevere_b<-case_vaccine_nonsevere_b[,c(-1,-2)]
+
+case_vaccine_c<-case_vaccine_c[,c(-1,-2)]
+case_vaccine_severe_c<-case_vaccine_severe_c[,c(-1,-2)]
+case_vaccine_nonsevere_c<-case_vaccine_nonsevere_c[,c(-1,-2)]
+
 ####################################################
 
 control_data<-fread(paste0(output_cov_pregnant_control, "pregnant_cov_neg_control.csv"))
-control_vaccine<-control_data$vaccine
+control_vaccine<-control_data[,c("severity", "covid_trimester", "vaccine")]
 control_data<-control_data[,-"vaccine"]
 
 control_data$any<-rowSums(control_data[,4:ncol(control_data)])
@@ -73,6 +97,17 @@ control_data_b<-control_data_b[,c(-1,-2,-3)]
 
 control_data_c<-control_data[control_data$covid_trimester==3,]
 control_data_c<-control_data_c[,c(-1,-2,-3)]
+
+##############################################
+
+control_vaccine_a<-control_vaccine[control_vaccine$covid_trimester==1,]
+control_vaccine_a<-control_vaccine_a[,c(-1,-2)]
+
+control_vaccine_b<-control_vaccine[control_vaccine$covid_trimester==2,]
+control_vaccine_b<-control_vaccine_b[,c(-1,-2)]
+
+control_vaccine_c<-control_vaccine[control_vaccine$covid_trimester==3,]
+control_vaccine_c<-control_vaccine_c[,c(-1,-2)]
 
 # no severity- these controls didn't have covid 
 
@@ -106,11 +141,32 @@ if((nrow(my_df)==0)==T){
 results<-paste0(total_cases_sums,"(", my_percent,"%)")}
 results_a[,(j+1)]<-results}
 
-vaccine_a<-vector(length=length(my_data))
+####################################################################
 
 
+my_vaccine<-list(case_vaccine_a, case_vaccine_severe_a, case_vaccine_nonsevere_a, control_vaccine_a)
+vaccine_a<-vector()
 
-results_a<-rbind(vaccine_a, any_result)
+for(j in 1:length(my_vaccine)){
+  my_df<-my_vaccine[[j]] 
+  print(j)
+  if((nrow(my_df)==0)==T){  
+    results<-rep("no records", length(my_names))
+    print(paste0(i,": no records in this group"))
+  }else{print("records found for this group")
+    if(nrow(my_df>1)){total_cases_sums<-colSums(my_df)} else{(total_cases_sums<-my_df[1,])}
+    
+    my_percent<-(total_cases_sums/nrow(my_df))
+    my_percent<-round(my_percent,3)
+    my_percent<-my_percent*100
+    
+    results<-paste0(total_cases_sums,"(", my_percent,"%)")}
+  vaccine_a[j]<-results}
+
+vaccine_a<-c("flu vaccine", vaccine_a)
+
+
+results_a<-rbind(results_a, vaccine_a)
 
 fwrite(results_a, paste0(final_output_dir, DAP,"_table2a_comorbid.csv"))
 
@@ -141,10 +197,32 @@ for(j in 1:length(my_data)){
     results<-paste0(total_cases_sums,"(", my_percent,"%)")}
   results_b[,(j+1)]<-results}
 
-vaccine_b<-vector(length=length(my_data))
+####################################################################
 
 
-results_b<-rbind(vaccine_b, any_result)
+my_vaccine<-list(case_vaccine_b, case_vaccine_severe_b, case_vaccine_nonsevere_b, control_vaccine_b)
+vaccine_b<-vector()
+
+for(j in 1:length(my_vaccine)){
+  my_df<-my_vaccine[[j]] 
+  print(j)
+  if((nrow(my_df)==0)==T){  
+    results<-rep("no records", length(my_names))
+    print(paste0(i,": no records in this group"))
+  }else{print("records found for this group")
+    if(nrow(my_df>1)){total_cases_sums<-colSums(my_df)} else{(total_cases_sums<-my_df[1,])}
+    
+    my_percent<-(total_cases_sums/nrow(my_df))
+    my_percent<-round(my_percent,3)
+    my_percent<-my_percent*100
+    
+    results<-paste0(total_cases_sums,"(", my_percent,"%)")}
+  vaccine_b[j]<-results}
+
+vaccine_b<-c("flu vaccine", vaccine_b)
+
+
+results_b<-rbind(results_b, vaccine_b)
 
 fwrite(results_b, paste0(final_output_dir, DAP,"_table2b_comorbid.csv"))
 
@@ -175,10 +253,31 @@ for(j in 1:length(my_data)){
     results<-paste0(total_cases_sums,"(", my_percent,"%)")}
   results_c[,(j+1)]<-results}
 
-vaccine_c<-vector(length=length(my_data))
+####################################################################
 
 
+my_vaccine<-list(case_vaccine_c, case_vaccine_severe_c, case_vaccine_nonsevere_c, control_vaccine_c)
+vaccine_c<-vector()
 
-results_c<-rbind(vaccine_c, any_result)
+for(j in 1:length(my_vaccine)){
+  my_df<-my_vaccine[[j]] 
+  print(j)
+  if((nrow(my_df)==0)==T){  
+    results<-rep("no records", length(my_names))
+    print(paste0(i,": no records in this group"))
+  }else{print("records found for this group")
+    if(nrow(my_df>1)){total_cases_sums<-colSums(my_df)} else{(total_cases_sums<-my_df[1,])}
+    
+    my_percent<-(total_cases_sums/nrow(my_df))
+    my_percent<-round(my_percent,3)
+    my_percent<-my_percent*100
+    
+    results<-paste0(total_cases_sums,"(", my_percent,"%)")}
+  vaccine_c[j]<-results}
+
+vaccine_c<-c("flu vaccine", vaccine_c)
+
+
+results_c<-rbind(results_c, vaccine_c)
 
 fwrite(results_c,paste0(final_output_dir, DAP,"_table2c_comorbid.csv"))
--- scripts/p_steps/create_table_3abc_comorbities.R
+++ scripts/p_steps/create_table_3abc_comorbities.R
@@ -19,19 +19,42 @@ CDM_source<-fread(paste0(path_CDM,"CDM_SOURCE.csv"))
 DAP<-CDM_source$data_access_provider_name
 
 case_data<-fread(paste0(output_cov_cases, "cases.csv"))
+
+case_vaccine<-case_data[,c("severity", "covid_trimester","vaccine")]
+case_data<-case_data[,-"vaccine"]
+
+# create "any comorbidity" column
+
+case_data$any<-rowSums(case_data[,4:ncol(case_data)])
+case_data$any[case_data$any>0]<-1
+
+
+###################################################
+# subset by trimester and severity
+
 case_data_a<-case_data[(case_data$covid_trimester==1),]
 case_data_severe_a<-case_data_a[case_data_a$severity==1,]
 case_data_nonsevere_a<-case_data_a[case_data_a$severity==0,]
 
+case_vaccine_a<-case_vaccine[(case_vaccine$covid_trimester==1),]
+case_vaccine_severe_a<-case_vaccine_a[case_vaccine_a$severity==1,]
+case_vaccine_nonsevere_a<-case_vaccine_a[case_vaccine_a$severity==0,]
 
 case_data_b<-case_data[case_data$covid_trimester==2,]
 case_data_severe_b<-case_data_b[case_data_b$severity==1,]
 case_data_nonsevere_b<-case_data_b[case_data_b$severity==0,]
 
+case_vaccine_b<-case_vaccine[(case_vaccine$covid_trimester==2),]
+case_vaccine_severe_b<-case_vaccine_b[case_vaccine_b$severity==1,]
+case_vaccine_nonsevere_b<-case_vaccine_b[case_vaccine_b$severity==0,]
 
 case_data_c<-case_data[case_data$covid_trimester==3,]
 case_data_severe_c<-case_data_c[case_data_c$severity==1,]
 case_data_nonsevere_c<-case_data_c[case_data_c$severity==0,]
+
+case_vaccine_c<-case_vaccine[(case_vaccine$covid_trimester==3),]
+case_vaccine_severe_c<-case_vaccine_c[case_vaccine_c$severity==1,]
+case_vaccine_nonsevere_c<-case_vaccine_c[case_vaccine_c$severity==0,]
 
 ###########################################
 # remove non-covariate columns
@@ -48,10 +71,34 @@ case_data_c<-case_data_c[,c(-1,-2,-3)]
 case_data_severe_c<-case_data_severe_c[,c(-1,-2,-3)]
 case_data_nonsevere_c<-case_data_nonsevere_c[,c(-1,-2,-3)]
 
+###############################################################
+
+case_vaccine_a<-case_vaccine_a[,c(-1,-2)]
+case_vaccine_severe_a<-case_vaccine_severe_a[,c(-1,-2)]
+case_vaccine_nonsevere_a<-case_vaccine_nonsevere_a[,c(-1,-2)]
+
+case_vaccine_b<-case_vaccine_b[,c(-1,-2)]
+case_vaccine_severe_b<-case_vaccine_severe_b[,c(-1,-2)]
+case_vaccine_nonsevere_b<-case_vaccine_nonsevere_b[,c(-1,-2)]
+
+case_vaccine_c<-case_vaccine_c[,c(-1,-2)]
+case_vaccine_severe_c<-case_vaccine_severe_c[,c(-1,-2)]
+case_vaccine_nonsevere_c<-case_vaccine_nonsevere_c[,c(-1,-2)]
+
+####################################################
 ####################################################
 
 
 control_data<-fread(paste0(output_cov_nonpregnant_control, "covid_positive_nonpregnant_control.csv"))
+
+control_vaccine<-control_data[,c("severity", "covid_trimester", "vaccine")]
+control_data<-control_data[,-"vaccine"]
+
+control_data$any<-rowSums(control_data[,4:ncol(control_data)])
+control_data$any[control_data$any>0]<-1
+
+
+
 control_data_a<-control_data[(control_data$covid_trimester==1),]
 control_data_severe_a<-control_data_a[control_data_a$severity==1,]
 control_data_nonsevere_a<-control_data_a[control_data_a$severity==0,]
@@ -65,6 +112,23 @@ control_data_nonsevere_b<-control_data_b[control_data_b$severity==0,]
 control_data_c<-control_data[control_data$covid_trimester==3,]
 control_data_severe_c<-control_data_c[control_data_c$severity==1,]
 control_data_nonsevere_c<-control_data_c[control_data_c$severity==0,]
+
+##############################################################
+
+control_vaccine_a<-control_vaccine[(control_vaccine$covid_trimester==1),]
+control_vaccine_severe_a<-control_vaccine_a[control_vaccine_a$severity==1,]
+control_vaccine_nonsevere_a<-control_vaccine_a[control_vaccine_a$severity==0,]
+
+
+control_vaccine_b<-control_vaccine[control_vaccine$covid_trimester==2,]
+control_vaccine_severe_b<-control_vaccine_b[control_vaccine_b$severity==1,]
+control_vaccine_nonsevere_b<-control_vaccine_b[control_vaccine_b$severity==0,]
+
+
+control_vaccine_c<-control_vaccine[control_vaccine$covid_trimester==3,]
+control_vaccine_severe_c<-control_vaccine_c[control_vaccine_c$severity==1,]
+control_vaccine_nonsevere_c<-control_vaccine_c[control_vaccine_c$severity==0,]
+
 
 ###########################################
 # remove non-covariate columns
@@ -80,6 +144,20 @@ control_data_nonsevere_b<-control_data_nonsevere_b[,c(-1,-2,-3)]
 control_data_c<-control_data_c[,c(-1,-2,-3)]
 control_data_severe_c<-control_data_severe_c[,c(-1,-2,-3)]
 control_data_nonsevere_c<-control_data_nonsevere_c[,c(-1,-2,-3)]
+
+##########################################
+
+control_vaccine_a<-control_vaccine_a[,c(-1,-2)]
+control_vaccine_severe_a<-control_vaccine_severe_a[,c(-1,-2)]
+control_vaccine_nonsevere_a<-control_vaccine_nonsevere_a[,c(-1,-2)]
+
+control_vaccine_b<-control_vaccine_b[,c(-1,-2)]
+control_vaccine_severe_b<-control_vaccine_severe_b[,c(-1,-2)]
+control_vaccine_nonsevere_b<-control_vaccine_nonsevere_b[,c(-1,-2)]
+
+control_vaccine_c<-control_vaccine_c[,c(-1,-2)]
+control_vaccine_severe_c<-control_vaccine_severe_c[,c(-1,-2)]
+control_vaccine_nonsevere_c<-control_vaccine_nonsevere_c[,c(-1,-2)]
 
 ####################################################
 
@@ -114,6 +192,33 @@ if((nrow(my_df)==0)==T){
 results<-paste0(total_cases_sums,"(", my_percent,"%)")}
 results_a[,(j+1)]<-results}
 
+####################################################################
+
+
+my_vaccine<-list(case_vaccine_a, case_vaccine_severe_a, case_vaccine_nonsevere_a, control_vaccine_a, control_vaccine_severe_a, control_vaccine_nonsevere_a)
+vaccine_a<-vector()
+
+for(j in 1:length(my_vaccine)){
+  my_df<-my_vaccine[[j]] 
+  print(j)
+  if((nrow(my_df)==0)==T){  
+    results<-rep("no records", length(my_names))
+    print(paste0(i,": no records in this group"))
+  }else{print("records found for this group")
+    if(nrow(my_df>1)){total_cases_sums<-colSums(my_df)} else{(total_cases_sums<-my_df[1,])}
+    
+    my_percent<-(total_cases_sums/nrow(my_df))
+    my_percent<-round(my_percent,3)
+    my_percent<-my_percent*100
+    
+    results<-paste0(total_cases_sums,"(", my_percent,"%)")}
+  vaccine_a[j]<-results}
+
+vaccine_a<-c("flu vaccine", vaccine_a)
+
+
+results_a<-rbind(results_a, vaccine_a)
+
 fwrite(results_a, paste0(final_output_dir, DAP,"_table3a_comorbid.csv"))
 
 #####################################################################
@@ -143,6 +248,34 @@ for(j in 1:length(my_data)){
     results<-paste0(total_cases_sums,"(", my_percent,"%)")}
   results_b[,(j+1)]<-results}
 
+####################################################################
+
+
+my_vaccine<-list(case_vaccine_b, case_vaccine_severe_b, case_vaccine_nonsevere_b, control_vaccine_b, control_vaccine_severe_b, control_vaccine_nonsevere_b)
+vaccine_b<-vector()
+
+for(j in 1:length(my_vaccine)){
+  my_df<-my_vaccine[[j]] 
+  print(j)
+  if((nrow(my_df)==0)==T){  
+    results<-rep("no records", length(my_names))
+    print(paste0(i,": no records in this group"))
+  }else{print("records found for this group")
+    if(nrow(my_df>1)){total_cases_sums<-colSums(my_df)} else{(total_cases_sums<-my_df[1,])}
+    
+    my_percent<-(total_cases_sums/nrow(my_df))
+    my_percent<-round(my_percent,3)
+    my_percent<-my_percent*100
+    
+    results<-paste0(total_cases_sums,"(", my_percent,"%)")}
+  vaccine_b[j]<-results}
+
+vaccine_b<-c("flu vaccine", vaccine_b)
+
+
+results_b<-rbind(results_b, vaccine_b)
+
+
 fwrite(results_b, paste0(final_output_dir, DAP,"_table3b_comorbid.csv"))
 
 #####################################################################
@@ -171,5 +304,33 @@ for(j in 1:length(my_data)){
     
     results<-paste0(total_cases_sums,"(", my_percent,"%)")}
   results_c[,(j+1)]<-results}
+
+####################################################################
+
+
+my_vaccine<-list(case_vaccine_c, case_vaccine_severe_c, case_vaccine_nonsevere_c, control_vaccine_c, control_vaccine_severe_c, control_vaccine_nonsevere_c)
+vaccine_c<-vector()
+
+for(j in 1:length(my_vaccine)){
+  my_df<-my_vaccine[[j]] 
+  print(j)
+  if((nrow(my_df)==0)==T){  
+    results<-rep("no records", length(my_names))
+    print(paste0(i,": no records in this group"))
+  }else{print("records found for this group")
+    if(nrow(my_df>1)){total_cases_sums<-colSums(my_df)} else{(total_cases_sums<-my_df[1,])}
+    
+    my_percent<-(total_cases_sums/nrow(my_df))
+    my_percent<-round(my_percent,3)
+    my_percent<-my_percent*100
+    
+    results<-paste0(total_cases_sums,"(", my_percent,"%)")}
+  vaccine_c[j]<-results}
+
+vaccine_c<-c("flu vaccine", vaccine_c)
+
+
+results_c<-rbind(results_c, vaccine_c)
+
 
 fwrite(results_c,paste0(final_output_dir, DAP,"_table3c_comorbid.csv"))
