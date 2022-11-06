54a4756526648d0c772f273d26b651dd5c0c8ddd
Ema <e.m.alsina-2@umcutrecht.nl>
2022-11-06 14:47:49 +0100

    cesarea codes for IACS = cesarea codes from codeslist

--- scripts/p_steps/IACS_mat_cov_no_link.R
+++ scripts/p_steps/IACS_mat_cov_no_link.R
@@ -43,7 +43,7 @@ for(i in 1:length(preg_cohort_folders)){
   GEST_DIAB_codelist<-all_codes[all_codes$event_match_name==my_event_name,]
   CreateConceptDatasets(codesheet = GEST_DIAB_codelist, fil=EVENTS, path = maternal_covariates_events)
   
-  GEST_DIAB_EV<-readRDS(paste0(maternal_covariates_eventsmy_file_name,".rds"))
+  GEST_DIAB_EV<-readRDS(paste0(maternal_covariates_events, my_file_name,".rds"))
   GEST_DIAB_EV_ID<-(GEST_DIAB_EV$person_id)
   GEST_DIAB_EV_Date<- (GEST_DIAB_EV$start_date_record)
   
@@ -64,12 +64,12 @@ for(i in 1:length(preg_cohort_folders)){
   CESAREA_codelist<-all_codes[all_codes$event_match_name==my_event_name,]
   CreateConceptDatasets(codesheet = CESAREA_codelist, fil=EVENTS, path = maternal_covariates_events)
   
-  CESAREA_EV<-readRDS(paste0(maternal_covariates_eventsmy_file_name,".rds"))
+  CESAREA_EV<-readRDS(paste0(maternal_covariates_events,my_file_name,".rds"))
   CESAREA_EV_ID<-(GEST_DIAB_EV$person_id)
   CESAREA_EV_Date<- (GEST_DIAB_EV$start_date_record)
   
   
-  my_rows<-which(PROC$origin_of_procedure=="CMBD"&PROC$procedure_code%in%CESAREA_codes)
+  my_rows<-which(PROC$origin_of_procedure=="CMBD"&PROC$procedure_code%in%CESAREA_codelist)
   CESAREA_PROC_ID<-PROC$person_id[my_rows]
   CESAREA_PROC_Date<-PROC$procedure_date[my_rows]
   
@@ -92,7 +92,7 @@ for(i in 1:length(preg_cohort_folders)){
   SPONTABO_codelist<-all_codes[all_codes$event_match_name==my_event_name,]
   CreateConceptDatasets(codesheet = SPONTABO_codelist, fil=EVENTS, path = maternal_covariates_events)
   
-  SPONTABO_EV<-readRDS(paste0(maternal_covariates_eventsmy_file_name,".rds"))
+  SPONTABO_EV<-readRDS(paste0(maternal_covariates_events,my_file_name,".rds"))
   SA_EV_ID<-(SPONTABO_EV$person_id)
   SA_EV_Date<- (SPONTABO_EV$start_date_record)
   
@@ -163,7 +163,7 @@ for(i in 1:length(preg_cohort_folders)){
   PREECLAMP_codelist<-all_codes[all_codes$event_match_name==my_event_name,]
   CreateConceptDatasets(codesheet = PREECLAMP_codelist, fil=EVENTS, path = maternal_covariates_events)
   
-  PREECLAMP_EV<-readRDS(paste0(maternal_covariates_eventsmy_file_name,".rds"))
+  PREECLAMP_EV<-readRDS(paste0(maternal_covariates_events,my_file_name,".rds"))
   PREECLAMP_EV_ID<-(PREECLAMP_EV$person_id)
   PREECLAMP_EV_Date<- (PREECLAMP_EV$start_date_record)
   
