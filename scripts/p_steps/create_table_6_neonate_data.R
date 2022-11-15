489e959920496e4631495a939e8a4a0fca55147b
Ema <e.m.alsina-2@umcutrecht.nl>
2022-11-15 08:10:49 +0100

    restrict table 6 neonates to those with nonsevere in utero covid exposure

--- scripts/p_steps/create_table_6_neonate_data.R
+++ scripts/p_steps/create_table_6_neonate_data.R
@@ -7,8 +7,8 @@
 # matneral covariates
 # covid comorbidities
 
-# glm(formula = outcome ~ case + cov_comorb+ mat_cov, family = "poisson", data = p)
 
+# need to pull in severity 
 
 # import maternal outcome data
 case_neo_outcome<-fread(paste0(output_neonates_case, "case_neonates_outcomes.csv"))
@@ -18,12 +18,14 @@ case_mom_data<-fread(paste0(case_neonate_folder,"case_neonates.csv"))
 case_mat_cov<-fread(paste0(g_output_mat_cov_pan_pos, "cases.csv"))
 case_mat_cov$mom_id<-case_mat_cov$person_id
 
-case_mat_cov<-case_mat_cov[,c("mom_id", "covid_trimester")]
+case_mat_cov<-case_mat_cov[,c("mom_id", "covid_trimester", "severity")]
 # neonates need trimester and mom id from their moms
 case_mom_data<-merge(case_mom_data, case_mat_cov, by="mom_id")
 
 case_neo_outcome<-merge(case_mom_data, case_neo_outcome, by="person_id")
 
+
+case_neo_outcome<-case_neo_outcome[case_neo_outcome$severity==0,]
 
 CDM_source<-fread(paste0(path_CDM,"CDM_SOURCE.csv"))
 DAP<-CDM_source$data_access_provider_name
