602ba6a92bf68b9116dee2d5bf72b16b3bc6cb0e
Ema <e.m.alsina-2@umcutrecht.nl>
2022-11-15 08:21:04 +0100

    preterm denominator and numerator implemented for table 6

--- scripts/p_steps/create_table_6_preterm.R
+++ scripts/p_steps/create_table_6_preterm.R
@@ -37,28 +37,40 @@ my_cols<-list(c(2:4), c(5:7), c(8:10))
 for(j in 1:3){
 input_col<-my_cols[[j]]
 my_preterm_data<-my_preterm_data_list[[j]]
-my_preterm_data<-my_preterm_data[my_preterm_data$preterm==1,]
+my_cohort_size<-nrow(my_preterm_data)
 
 for(i in 1:length(my_drug_names)){
 results<-list()
-my_drug_data<-fread(paste0(output_cov_window_atc_2, my_drug_files[[6]]))
+my_drug_data<-fread(paste0(output_cov_window_atc_2, my_drug_files[[i]]))
 my_drug_data<-my_drug_data[my_drug_data$cov_trimester==j,]
-my_denom<-nrow(my_drug_data)
-if(my_denom>0){
+# make sure drug exposure columns are numeric
+my_drug_data$minus_30<-as.numeric(my_drug_data$minus_30)
+my_drug_data$plus_30<-as.numeric(my_drug_data$plus_30)
+# need to match the ID because not all  pregnancies will be in LB
+my_drug_data<-my_drug_data[my_drug_data$person_id%in%my_preterm_data$person_id,]
+my_denom_A<-sum(my_drug_data$minus_30)
+my_denom_B<-(my_cohort_size)-(sum(my_drug_data$plus_30))
+my_denom_C<-sum(my_drug_data$plus_30)
+
+# select only those with maternal outcome
+my_preterm_data<-my_preterm_data[my_preterm_data$preterm==1,]
 my_preterm_drug<-merge(my_preterm_data, my_drug_data, by="person_id")
 my_numer_minus30<-nrow(my_preterm_drug[my_preterm_drug$minus_30==1,])
 my_numer_plus30<-nrow(my_preterm_drug[my_preterm_drug$plus_30==1,])
 my_numer_nonexposed_plus30<-nrow(my_preterm_drug[my_preterm_drug$plus_30==0,])
 
-  my_prop_test_minus30<-prop.test(my_numer_minus30, my_denom)
+if(my_denom_A>0){
+  my_prop_test_minus30<-prop.test(my_numer_minus30, my_denom_A)
   results[[1]]<-paste0((round(my_prop_test_minus30$estimate,3)*100)," (", (round(my_prop_test_minus30$conf.int[1],3)*100),"-",(round(my_prop_test_minus30$conf.int[2],3)*100),")")
-  my_prop_test_nonexposed_plus30<-prop.test(my_numer_nonexposed_plus30, my_denom)
+  }else{results[[1]]<-"no matches"}  
+if(my_denom_A>0){  
+  my_prop_test_nonexposed_plus30<-prop.test(my_numer_nonexposed_plus30, my_denom_B)
   results[[2]]<-paste0((round(my_prop_test_nonexposed_plus30$estimate,3)*100)," (", (round(my_prop_test_nonexposed_plus30$conf.int[1],3)*100),"-",(round(my_prop_test_nonexposed_plus30$conf.int[2],3)*100),")")
-  my_prop_test_plus30<-prop.test(my_numer_plus30, my_denom)
+  }else{results[[2]]<-"no matches"}  
+if(my_denom_A>0){
+  my_prop_test_plus30<-prop.test(my_numer_plus30, my_denom_C)
   results[[3]]<-paste0((round(my_prop_test_plus30$estimate,3)*100)," (", (round(my_prop_test_plus30$conf.int[1],3)*100),"-",(round(my_prop_test_plus30$conf.int[2],3)*100),")")
-}else{results[[1]]<-"no matches"
-results[[2]]<-"no matches"
-results[[3]]<-"no matches"}
+  }else{results[[3]]<-"no matches"}
 DU_preterm_outcome[i,input_col]<-unlist(results)
 }
 
