8950f08e566f7e307b573422e2cfef08b43f6519
Ema <e.m.alsina-2@umcutrecht.nl>
2023-05-30 17:27:21 +0200

    neonatal tables need cleared env and user input re-entry

--- MAIN_to_run.R
+++ MAIN_to_run.R
@@ -416,6 +416,23 @@ source("params.R")
 
 source("99_path.R")
 
+#USER INPUT
+#if your CDM is stored in a different folder than the projectFolder set to TRUE
+
+set_my_CDM<-FALSE
+
+if(set_my_CDM==F){
+  path_CDM<-paste0(projectFolder,"/CDMInstances/")
+  invisible(if(dir.exists(path_CDM)==F)
+  {dir.create(path_CDM)})}
+
+#if set_my_CDM<-TRUE enter the location of of your CDM 
+my_path_CDM<-"text string with location of your CDM"
+
+if(set_my_CDM==T){path_CDM<-paste0(my_path_CDM)
+invisible(if(dir.exists(path_CDM)==F)
+{dir.create(path_CDM)})}
+
 source(paste0(pre_dir, "/create_table_6_neonate_data.R"))
 
 source(paste0(pre_dir, "/create_table_6_LBW.R"))
